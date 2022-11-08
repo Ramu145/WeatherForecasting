3c925ff188ae85fd64ca5194f95e18ddec96385b
Ramu145 <samudrala.ramu145@gmail.com>
2022-11-08 13:25:02 +0000

    completed Designing and Development and committed.

--- src/app/forecasting/weather/weather.component.ts
+++ src/app/forecasting/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './../../services/api.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-weather',
@@ -11,6 +12,7 @@ export class WeatherComponent implements OnInit {
   selectedCityVal = false;
   validateLatlng = false;
   weatherDetails: any;
+
   // lat: any = 51.5464785;
   // lon: any = 0.0549158;
   // aPIKey: any = 'fe3695759da76e0c9dcaf566634a08ed';
@@ -26,7 +28,7 @@ export class WeatherComponent implements OnInit {
     searchedCity: new FormControl(''),
   });
 
-  constructor(private service: ApiService) {}
+  constructor(private service: ApiService, private router: Router) {}
 
   ngOnInit(): void {
     // this.api.getData('https://api.openweathermap.org/data/2.5/forecast?lat=51.5464785&lon=0.0549158&appid=fe3695759da76e0c9dcaf566634a08ed').subscribe((data) => {
@@ -58,12 +60,18 @@ export class WeatherComponent implements OnInit {
       this.weatherDetails = data;
       if (data) {
         this.weatherDetails.list.forEach((element: any, index: number) => {
-          //let day = dayName[new Date().getDay()];
-          //   console.log(element);
-
           element.id = 'element' + index;
-
           element.dayName = dayName[new Date(element.dt_txt).getDay()];
+          element.main.temp_min = (element.main.temp_min - 273.15).toFixed(2);
+          element.main.temp_max = (element.main.temp_max - 273.15).toFixed(2);
+          element.windSpeedIcon = 'assets/wind.png';
+          if (element.weather[0].main === 'Rain') {
+            element.icon = 'assets/rain.png';
+          } else if (element.weather[0].main === 'Clouds') {
+            element.icon = 'assets/cloudy.png';
+          } else if (element.weather[0].main === 'Clear') {
+            element.icon = 'assets/sun.png';
+          }
         });
       }
       console.log('================================', this.weatherDetails.list);
@@ -72,5 +80,13 @@ export class WeatherComponent implements OnInit {
 
   onSubmit() {
     this.service.onSubmit(this.weatherForm.value);
+    if (this.weatherDetails.list) {
+      this.selectedCityVal = false;
+      this.validateLatlng = false;
+      setTimeout(() => {
+        this.selectedCityVal = true;
+        this.validateLatlng = true;
+      }, 200);
+    }
   }
 }
